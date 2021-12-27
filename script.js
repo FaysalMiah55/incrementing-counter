003ef6e5b0818e656c1334d39636478372b64a26
FaysalMiah55 <faysal.miah@northsouth.edu>
2021-12-27 21:26:24 +0600

    Increment Counter

---
+++ script.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const num = +counter.innerText;
+
+    const increment = target / 200;
+
+    if (num < target) {
+      counter.innerText = `${Math.ceil(num + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
